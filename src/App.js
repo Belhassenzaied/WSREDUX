94b57a13e6df1c223f6e2254f69548fc0f7cd1ed
belhassenzaied <zaied.voo@gmail.com>
2024-09-25 15:26:13 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AddTask from './Components/AddTask';
+import ListTask from './Components/ListTask';
+
+function App() {
+  return (
+    <div>
+      <ListTask/>
+      <AddTask/>
+    </div>
+  );
+}
+
+export default App;
